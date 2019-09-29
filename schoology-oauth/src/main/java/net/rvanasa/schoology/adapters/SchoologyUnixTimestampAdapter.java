957d3d3d093958ea77b8698157ef328a94815c1f
Mitchell Sulkowski <arduinoboy2560@gmail.com>
2019-09-29 00:31:22 -0400

    Event objects

--- schoology-oauth/src/main/java/net/rvanasa/schoology/adapters/SchoologyUnixTimestampAdapter.java
+++ schoology-oauth/src/main/java/net/rvanasa/schoology/adapters/SchoologyUnixTimestampAdapter.java
@@ -1,6 +1,8 @@
 package net.rvanasa.schoology.adapters;
 
 import java.lang.reflect.Type;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import com.google.gson.JsonDeserializationContext;
@@ -10,11 +12,29 @@ import com.google.gson.JsonParseException;
 
 public class SchoologyUnixTimestampAdapter implements JsonDeserializer<Date>
 {
-
+	
+	private final SimpleDateFormat dateTimeFormat = new SimpleDateFormat("YYYY-MM-DD HH:mm:ss");
+	
 	@Override
 	public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) 
 			throws JsonParseException {
-		return new Date(json.getAsLong());
+		
+		String raw = json.getAsString();
+		
+		try {
+			//Assume unix time
+			long l = Long.parseLong(raw);
+			
+			return new Date(l);
+		} catch (Exception e) {
+			//Assume datetime format
+			try {
+				return dateTimeFormat.parse(raw);
+			} catch (ParseException e1) {
+				return null;
+			}
+		}
+		
 	}
 
 }
