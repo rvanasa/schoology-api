84c986034b47e3e12e31e186c7ce084402f2f4d8
Mitchell Sulkowski <arduinoboy2560@gmail.com>
2019-10-01 16:42:05 -0400

    Fix getter

--- schoology-api/src/main/java/net/rvanasa/schoology/obj/sections/SchoologyCourseSectionsPage.java
+++ schoology-api/src/main/java/net/rvanasa/schoology/obj/sections/SchoologyCourseSectionsPage.java
@@ -1,11 +1,13 @@
 package net.rvanasa.schoology.obj.sections;
 
+import lombok.Getter;
 import net.rvanasa.schoology.obj.SchoologyPage;
 
 /*
  * https://developers.schoology.com/api-documentation/rest-api-v1/course-section
  * Represents a page of sections for given course
  */
+@Getter
 public class SchoologyCourseSectionsPage extends SchoologyPage
 {
 	
